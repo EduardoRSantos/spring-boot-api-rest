0f67a4000a7a5dce30daef740618629d5538efc3
EduardoRSantos <eduardoow6@gmail.com>
2024-02-05 21:29:07 -0300

    Book model create

---
+++ src/main/java/br/com/erudio/apiresterudio/models/Book.java
@@ -0,0 +1,23 @@
+package br.com.erudio.apiresterudio.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.DateTimeException;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String author;
+    @Column(name = "launch_date")
+    private Date launchDate;
+    private Double price;
+    private String title;
+
+}
