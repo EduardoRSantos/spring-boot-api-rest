7d1bfbbf8ccecfda148f7ee54f26444a3add01ab
EduardoRSantos <eduardoow6@gmail.com>
2024-02-09 19:45:05 -0300

    Mapper Book

--- src/main/java/br/com/erudio/apiresterudio/mapper/custom/MapperCustom.java
+++ src/main/java/br/com/erudio/apiresterudio/mapper/custom/MapperCustom.java
@@ -1,6 +1,8 @@
 package br.com.erudio.apiresterudio.mapper.custom;
 
+import br.com.erudio.apiresterudio.data.vo.v1.BookVo;
 import br.com.erudio.apiresterudio.data.vo.v1.PersonVo;
+import br.com.erudio.apiresterudio.models.Book;
 import br.com.erudio.apiresterudio.models.Person;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,8 @@ public class MapperCustom {
     static {
         mapper.createTypeMap(Person.class, PersonVo.class).addMapping(Person::getId, PersonVo::setKey);
         mapper.createTypeMap(PersonVo.class, Person.class).addMapping(PersonVo::getKey, Person::setId);
+        mapper.createTypeMap(Book.class, BookVo.class).addMapping(Book::getId, BookVo::setKey);
+        mapper.createTypeMap(BookVo.class, Book.class).addMapping(BookVo::getKey, Book::setId);
     }
     public static <O, D> D parseObject(O origin, Class<D> destination) {
         return mapper.map(origin, destination);
