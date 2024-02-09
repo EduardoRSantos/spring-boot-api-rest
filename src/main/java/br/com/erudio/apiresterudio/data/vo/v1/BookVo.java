875631d3db6dd7f0b98bc048969abadd334b7bb7
EduardoRSantos <eduardoow6@gmail.com>
2024-02-09 19:44:46 -0300

    BookDto

--- src/main/java/br/com/erudio/apiresterudio/data/vo/v1/BookVo.java
+++ src/main/java/br/com/erudio/apiresterudio/data/vo/v1/BookVo.java
@@ -3,18 +3,19 @@ package br.com.erudio.apiresterudio.data.vo.v1;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
-@JsonPropertyOrder({"id", "author", "lauchDate", "price", "title"})
-public class BookVo implements Serializable {
+@JsonPropertyOrder({"id", "author", "launch_date", "price", "title"})
+public class BookVo extends RepresentationModel<BookVo> implements Serializable {
 
     @JsonProperty("id")
     private Integer Key;
     private String author;
-    private Date lauchDate;
+    private Date launchDate;
     private Double price;
     private String title;
 
